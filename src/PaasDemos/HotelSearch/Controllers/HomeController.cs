0ea67e7763c7982c09a36c0b1adf2dd882380b7c
kentkingery <kent@acmecode.io>
2021-03-21 17:45:30 -0500

    Add src folder

---
+++ src/PaasDemos/HotelSearch/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using HotelSearch.Models;
+
+
+namespace HotelSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Search(SearchData model)
+         {
+            try
+            {
+                // Ensure the search string is valid.
+                if (model.searchText == null)
+                {
+                    model.searchText = "";
+                }
+
+                // Make the Azure Cognitive Search call.
+                await RunQueryAsync(model).ConfigureAwait(false);
+            }
+            catch
+            {
+                return View("Error", new ErrorViewModel { RequestId = "1" });
+            }
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static SearchClient _searchClient;
+        private static SearchIndexClient _indexClient;
+        private static IConfigurationBuilder _builder;
+        private static IConfigurationRoot _configuration;
+
+        private void InitSearch()
+        {
+            // Create a configuration using the appsettings file.
+            _builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            _configuration = _builder.Build();
+
+            // Pull the values from the appsettings.json file.
+            string searchServiceUri = _configuration["SearchServiceUri"];
+            string queryApiKey = _configuration["SearchServiceQueryApiKey"];
+
+            // Create a service and index client.
+            _indexClient = new SearchIndexClient(new Uri(searchServiceUri), new AzureKeyCredential(queryApiKey));
+            _searchClient = _indexClient.GetSearchClient("hotels-index");
+        }
+
+        private async Task<ActionResult> RunQueryAsync(SearchData model)
+        {
+            InitSearch();
+
+            var options = new SearchOptions()
+            {
+                IncludeTotalCount = true
+            };
+
+            // Enter Hotel property names into this list so only these values will be returned.
+            // If Select is empty, all values will be returned, which can be inefficient.
+            options.Select.Add("HotelName");
+            options.Select.Add("Description");
+            options.Select.Add("Category");
+            options.Select.Add("Rating");
+            options.Select.Add("Address");
+            // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
+            model.resultList = await _searchClient.SearchAsync<Hotel>(model.searchText, options).ConfigureAwait(false);
+
+            // Display the results.
+            return View("Search", model);
+        }
+    }
+}
